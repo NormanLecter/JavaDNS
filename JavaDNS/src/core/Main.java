c64197ec84688ffa2eafc0d3fd08148e03e86b84
Łukasz <ukasz@DESKTOP-4N4EMMD>
2016-11-10 10:34:17 +0100

    Test version - client - LW

--- JavaDNS/src/core/Main.java
+++ JavaDNS/src/core/Main.java
@@ -7,7 +7,28 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Java DNS client.");
+		try{
+			DatagramSocket client = new DatagramSocket(); // socket do przesylania i odbierania  datagramow
+			InetAddress address = InetAddress.getByName("192.168.41.106"); // klasa przechowujaca adres IP
 		
+			byte[] sendbyte = new byte[1024]; // konterner wysylanych danych
+			byte[] receivebyte = new byte[1024]; // konterner odbierania danych
+			
+			BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Wprowadz domene albo adres IP... ");
+            String domain = in.readLine();
+            sendbyte = domain.getBytes();
+			DatagramPacket sender = new DatagramPacket(sendbyte, sendbyte.length, address, 1309); // spakowanie wszystkiego do wyslania
+			client.send(sender);
+            
+			DatagramPacket receiver = new DatagramPacket(receivebyte,receivebyte.length);
+            client.receive(receiver);
+            String data =new String(receiver.getData());
+            System.out.println("IP adres / nazwa domeny : "+ data.trim()); // odczytanie usuwajac biale znaki
+            client.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
 	}
 }

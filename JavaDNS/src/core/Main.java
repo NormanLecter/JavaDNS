a67810874531bff6c904691fddcb9ba06d9061c5
Łukasz <ukasz@DESKTOP-4N4EMMD>
2016-11-10 09:40:32 +0100

    Commit to master branch - LW

--- JavaDNS/src/core/Main.java
+++ JavaDNS/src/core/Main.java
@@ -1,7 +1,10 @@
 package core;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Java DNS client.;;");;;;
+		System.out.println("Java DNS client.");
 	}
 }

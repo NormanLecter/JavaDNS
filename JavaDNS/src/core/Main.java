f01c000f7db3090ec8a7aa3625352e10e7ada340
Grzegorz Osak <g4rhold@gmail.com>
2016-11-16 21:59:56 +0100

    Respond with level of domain.

--- JavaDNS/src/core/Main.java
+++ JavaDNS/src/core/Main.java
@@ -7,7 +7,10 @@ import java.net.InetAddress;
 public class Main {
 	public static void main(String args[]) {
 		boolean serverRunning = true;
-		System.out.println("start");
+		
+		String s2 = "1 www.google.com";
+		System.out.println(getNameOfSite(s2));
+		
 	    try {
 	    	DatagramSocket server = new DatagramSocket(53);
 	    	
@@ -22,14 +25,41 @@ public class Main {
 	            String s = str.trim();
 	            
 	            System.out.println("odebralem: " + s);
+	            int levelOfDomain = getLevelOfDomain(s);
+	            String adress = getAdress(s);
+	            String domain = getDomain(s);
 	            
 	            InetAddress addr = receiver.getAddress();
 	            int port = receiver.getPort();
 	            
-	            String ip[] = {"123.123.10.20", "321.321.01.02"};
-	            String name[] = {"www.google.com", "www.jajestem.com"};
+	            String first[] = {"org", "pl"};
+	            String org[] = {"joemonster.org", "wikipedia.org"};
+	            String pl[] = {"wolniak.pl", "osak.pl"};
+
 	            
-	            for(int i = 0 ; i < ip.length; i++) {
+	            switch(levelOfDomain) {
+	            case 0: {
+	            	// sprawdzanie czy istnieje taka .domena je¿eli tak to odes³anie ze zwiêkszeniem levelOfDomain
+	            	
+	            	levelOfDomain++;
+	            	String msg = getMessage(levelOfDomain, adress);
+	            	break; }
+	            	
+	            case 2: {
+	            	// sprawdzenie czy istnieje taka strona.domena je¿eli tak to odes³anie ze zwiêkszeniem levelOfDomain
+	            	levelOfDomain++;
+	            	String msg = getMessage(levelOfDomain, adress);
+	            	break; }
+	            	
+	            case 4: {
+	            	// odnalezienie adresu IP strony i odes³anie wrz z levelOfDomain = 0;
+	            	levelOfDomain++;
+	            	String msg = getMessage(0, adress);
+	            	break; }
+	            }
+	            
+	            /*
+	            for(int i = 0 ; i < first.length; i++) {
 	            	if(s.equals(ip[i])) {
 	            		sendbyte = name[i].getBytes();
 	                    DatagramPacket sender= new DatagramPacket(sendbyte, sendbyte.length, addr, port);
@@ -42,11 +72,77 @@ public class Main {
 	                    break;
 	                }   
 	            }         
-	            break;
+	            break; 
+	            */  
 	       }
 	    } catch(Exception e) {
 	    	System.out.println(e);
 	    }
 	    System.out.println("end");
     }
+	
+	private static String getMessage(int level, String adress) {
+		String s = "";
+		s += Integer.toString(level);
+		s += " ";
+		s += adress;
+		return s;
+	}
+	
+	private static int getLevelOfDomain(String received) {
+		String s = "";
+		for(Character c : received.toCharArray()) {
+			if(c != ' ') {
+				s += c;
+			} else break;
+		}
+		
+		return Integer.parseInt(s);
+	}
+	
+	private static String getAdress(String received) {
+		StringBuffer s = new StringBuffer();
+		char[] array = received.toCharArray();
+		for(int i = array.length-1; i >= 0; i--) {
+			if(array[i] != ' ') {
+				s.append(array[i]);
+			} else break;
+		}
+		s.reverse();
+		return s.toString();
+	}
+	
+	private static String getDomain(String received) {
+		int dots = 0;
+		
+		for(Character c : received.toCharArray()) 
+			if(c == '.') 
+				dots++;
+		
+		StringBuffer s = new StringBuffer();
+		char[] array = received.toCharArray();
+		for(int i = array.length-1; i >= 0; i--) {
+			if(dots <= 1) {
+				s.deleteCharAt(s.length()-1);
+				break;
+			}
+			
+			s.append(array[i]);
+			
+			if(array[i] == '.') 
+				dots--;
+		}
+		s.reverse();
+		
+		return s.toString();
+	}
+	
+	private static String getNameOfSite(String received) {
+		StringBuffer s = new StringBuffer();
+		s.append(getAdress(received));
+		for(int i = 0; i < 4; i++)
+			s.deleteCharAt(0);
+		
+		return s.toString();
+	}
 }

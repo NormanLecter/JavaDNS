8b48b14edee3cbfcf3e741748edc1e238c5c8c2a
Łukasz <ukasz@192.168.1.11>
2016-11-16 19:21:27 +0100

    Add .proto file

--- JavaDNS/src/core/Main.java
+++ JavaDNS/src/core/Main.java
@@ -12,21 +12,21 @@ public class Main {
 			DatagramSocket client = new DatagramSocket(); // socket do przesylania i odbierania  datagramow
 			InetAddress address = InetAddress.getByName("192.168.41.106"); // klasa przechowujaca adres IP
 		
-			byte[] sendbyte = new byte[1024]; // konterner wysylanych danych
-			byte[] receivebyte = new byte[1024]; // konterner odbierania danych
+			byte[] sendbyte = new byte[1024]; // konterner wysylanych danych w bajtach
+			byte[] receivebyte = new byte[1024]; // konterner odbierania danych w bajtach
 			
 			BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Wprowadz domene albo adres IP... ");
+            System.out.println("Enter domain or IP address... ");
             String domain = in.readLine();
             sendbyte = domain.getBytes();
-			DatagramPacket sender = new DatagramPacket(sendbyte, sendbyte.length, address, 53); // spakowanie wszystkiego do wyslania
+			DatagramPacket sender = new DatagramPacket(sendbyte, sendbyte.length, address, 53); // 'spakowanie' wszystkiego do wyslania
 			client.send(sender);
             
 			//ODBIERANIE
 			DatagramPacket receiver = new DatagramPacket(receivebyte,receivebyte.length);
             client.receive(receiver);
             String data =new String(receiver.getData());
-            System.out.println("IP adres / nazwa domeny : "+ data.trim()); // odczytanie usuwajac biale znaki
+            System.out.println("IP address / domain : "+ data.trim()); // odczytanie usuwajac biale znaki
             client.close();
 		}
 		catch(Exception e){

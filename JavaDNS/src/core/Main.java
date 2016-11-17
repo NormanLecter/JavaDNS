43e81988f994e2faf2f4848cdef2f17c6b5ff9c6
Administrator <Administrator@c-07-2.std.cie.put.poznan.pl>
2016-11-17 10:13:59 +0100

    D

--- JavaDNS/src/core/Main.java
+++ JavaDNS/src/core/Main.java
@@ -6,9 +6,12 @@ import java.util.*;
 
 
 public class Main {
+	private static String adress;
+	private static String received;
 	
 	private static int getLevelOfDomain(String received) {
 		String s = "";
+		received = "0 1";
 		for(Character c : received.toCharArray()) {
 			if(c != ' ') {
 				s += c;
@@ -24,47 +27,68 @@ public class Main {
 		for(int i = array.length-1; i >= 0; i--) {
 			if(array[i] != ' ') {
 				s.append(array[i]);
-			} else break;
+			} else break;;
 		}
 		s.reverse();
 		return s.toString();
 	}
 	
 	public static void main(String[] args) {
-		// Z GITA CALY KATALOG COM.GOOGLE.PROTOBUF : package com.google.protobuf
-		try{
-			// WYSYLANIE
-			//websiteData ONET = websiteData.newBuilder().setIpAddress("192.168.1.2").setName("Onet").setLevelOfDomain(0).build(); // test protoBuffer
-			//byte[] webSend = ONET.newBuilderForType().toByteArray(); ===> BRAKUJE? ALBO BEZ TEGO I W DATAGRAM WRZUCIC LEVEL_OF_DOMAIN?
-			
-			DatagramSocket client = new DatagramSocket(); // socket do przesylania i odbierania  datagramow
-			InetAddress address = InetAddress.getByName("192.168.41.106"); // klasa przechowujaca adres IP
+		adress = "";
 		
-			byte[] sendbyte = new byte[1024]; // konterner wysylanych danych w bajtach
-			byte[] receivebyte = new byte[1024]; // konterner odbierania danych w bajtach
+		// Z GITA CALY KATALOG COM.GOOGLE.PROTOBUF : package com.google.protobuf
+		while(getLevelOfDomain(received) == 0 || getLevelOfDomain(received) == -1) {
+			try{
+				// WYSYLANIE
+				//websiteData ONET = websiteData.newBuilder().setIpAddress("192.168.1.2").setName("Onet").setLevelOfDomain(0).build(); // test protoBuffer
+				//byte[] webSend = ONET.newBuilderForType().toByteArray(); ===> BRAKUJE? ALBO BEZ TEGO I W DATAGRAM WRZUCIC LEVEL_OF_DOMAIN?
+				
+				DatagramSocket client = new DatagramSocket(); // socket do przesylania i odbierania  datagramow
+				InetAddress address = InetAddress.getByName("192.168.41.106"); // klasa przechowujaca adres IP
 			
-			BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Enter domain or IP address AND level od domain... ");
-            int k = 0;
-            String domain = "";
-            domain += k;
-            domain += " "; 
-            domain += in.readLine();
-            sendbyte = domain.getBytes();
-			DatagramPacket sender = new DatagramPacket(sendbyte, sendbyte.length, address, 53); // 'spakowanie' wszystkiego do wyslania
-			client.send(sender);
-            
-			//ODBIERANIE
-            int levelOfDomain = 0;
-			DatagramPacket receiver = new DatagramPacket(receivebyte,receivebyte.length);
-            client.receive(receiver);
-            String data =new String(receiver.getData());
-            
-            System.out.println("IP address / domain : "+ data.trim()); // odczytanie usuwajac biale znaki
-            client.close();
+				byte[] sendbyte = new byte[1024]; // konterner wysylanych danych w bajtach
+				byte[] receivebyte = new byte[1024]; // konterner odbierania danych w bajtach
+				
+				BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+	            System.out.println("Enter domain or IP address AND level od domain... ");
+	            
+	            if(adress.length() < 1) {
+	            	adress = in.readLine();
+		            int k = 0;
+		            String domain = "";
+		            domain += k;
+		            domain += " "; 
+		            domain += adress;
+		            sendbyte = domain.getBytes();
+					DatagramPacket sender = new DatagramPacket(sendbyte, sendbyte.length, address, 53); // 'spakowanie' wszystkiego do wyslania
+					client.send(sender);
+	            }
+				//ODBIERANIE
+				DatagramPacket receiver = new DatagramPacket(receivebyte,receivebyte.length);
+	            client.receive(receiver);
+	            String data = new String(receiver.getData());
+	            
+	            int levelOfDomain = getLevelOfDomain(data);
+	            levelOfDomain++;
+	            String msg = getMessage(levelOfDomain, adress);
+	            
+	            sendbyte = msg.getBytes();
+				DatagramPacket sende2r = new DatagramPacket(sendbyte, sendbyte.length, address, 53); // 'spakowanie' wszystkiego do wyslania
+				client.send(sende2r);
+	                
+	            client.close();
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
 		}
-		catch(Exception e){
-			System.out.println(e);
-		}
+	}
+	
+	private static String getMessage(int level, String adress) {
+		String s = "";
+		s += Integer.toString(level);
+		s += " ";
+		s += adress;
+		return s;
 	}
 }

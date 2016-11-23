f96994f8c05f1da3d1b9addb6c94c100ef69339e
Łukasz <ukasz@DESKTOP-4N4EMMD>
2016-11-23 11:12:08 +0100

    Adornment :)

--- JavaDNS/src/core/Main.java
+++ JavaDNS/src/core/Main.java
@@ -5,48 +5,39 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
-import com.google.protobuf.InvalidProtocolBufferException;
-
 import core.ProtosMessageDNS.MessageDNS;
 import core.ProtosMessageDNS.MessageDNS.Answer;
+import com.google.protobuf.InvalidProtocolBufferException;
  
 public class Main {
 	private static final int MAX_ANSWER_SIZE = 2048;
-	
 	private static String domain = "";
-	private static InetAddress addr;
+	private static InetAddress addressIP;
 	private static int port = 53;
 	private static int levelOfDomain = 0;
         
     public static void main(String[] args) {
     	try {
-        	addr = InetAddress.getByName("150.254.144.3");
-        	
+        	addressIP = InetAddress.getByName("150.254.144.3");
     		DatagramSocket client = new DatagramSocket(); 
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    		
     		while(levelOfDomain != -1) {
     			if(domain.length() < 1) {
     				System.out.println("Enter domain or IP address...");
-    				
     				domain = in.readLine();
-    				
+    				System.out.println();
     				if(domain.length() > MAX_ANSWER_SIZE) {
     					domain = "";
-    					System.out.println("Address is too long.");
+    					System.out.println("Domain address is too long, please try again.");
     					continue;
     				}
-    				
     				send(client);
     			}
-	       		receive(client);                                
-				      
+	       		receive(client);                                   
 	    		if(levelOfDomain == 0) {
 	    			domain = "";
 	    			continue;
 	    		}
-	    			
 	    		levelOfDomain++;
 	    		fullMsg();
 	    		send(client);
@@ -58,20 +49,15 @@ public class Main {
     
 	private static void receive(DatagramSocket server) throws IOException {
 		byte[] receivebyte = new byte[MAX_ANSWER_SIZE + 6];
-
         DatagramPacket receiver = new DatagramPacket(receivebyte, receivebyte.length);
         server.receive(receiver);
-        
-        addr = receiver.getAddress();
+        addressIP = receiver.getAddress();
         port = receiver.getPort();
-        
 		try {
 			Answer answer = MessageDNS.Answer.parseFrom(receivebyte);
 			levelOfDomain = answer.getTTL();
 			domain = answer.getNAME();
-			
-			System.out.println(port + " <- " + addr + " send: " + levelOfDomain + " " + domain);
-		
+			System.out.println("	<- port: "+ port + " address: " + addressIP + " send: " + " level of domain - "+ levelOfDomain + " message - " + domain);
 		} catch (InvalidProtocolBufferException e1) {
 			e1.printStackTrace();
 		}
@@ -83,10 +69,9 @@ public class Main {
         answerSend.setNAME(domain);
         answerSend.setTTL(levelOfDomain);
         byte[] bytes = answerSend.build().toByteArray();
-
-        DatagramPacket sender = new DatagramPacket(bytes, bytes.length, addr, port);
+        DatagramPacket sender = new DatagramPacket(bytes, bytes.length, addressIP, port);
         server.send(sender);
-        System.out.println(port + " -> " + addr + " send: " + answerSend.getTTL() + " " + answerSend.getNAME());
+        System.out.println("-> port: " + port + " address: "+ addressIP + " send: " + " level of domain - " +answerSend.getTTL() + " message - " + answerSend.getNAME());
 	}
 	
     private static void fullMsg() {
